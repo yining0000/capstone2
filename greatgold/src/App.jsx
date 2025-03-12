39be698edfe2aaa25364d26253e5a47ffc30fce1
yining0000 <wangyining@cityuniversity.edu>
2025-03-12 23:33:19 +0000

    updated

--- greatgold/src/App.jsx
+++ greatgold/src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
 import Payment from "./pages/Payment";
 
 const App = () => {
   return (
     <Router>
       <nav className="p-4 bg-gray-200">
-        <Link to="/" className="mr-4">Home</Link>
         <Link to="/payment">Payment</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
     </Router>
